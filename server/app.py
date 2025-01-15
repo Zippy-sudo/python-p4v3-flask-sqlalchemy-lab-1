7c53a541de043b75069cbc0763ecda5e0dbe7e8e
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2025-01-15 23:54:03 +0300

    First Trial

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response
+from models import Earthquake
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +22,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
 
+    if earthquake:
+        earthquake_obj = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+            }
+       
+        response = make_response(earthquake_obj,200)
+        return response
+    else:
+        return make_response({'message':f'Earthquake {id} not found.'},404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return make_response({'count':len(earthquakes), 'quakes':[{'id': earthquake.id, 'location':earthquake.location, 'magnitude':earthquake.magnitude, 'year':earthquake.year} for earthquake in earthquakes]},200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
